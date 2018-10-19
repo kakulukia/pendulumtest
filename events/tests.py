5ea0ab768dea897629bf6e57f107b28714e29a27
Andy Grabow <andy@freilandkiwis.de>
2018-10-19 14:22:36 +0200

    enhanced test cases

--- events/tests.py
+++ events/tests.py
@@ -6,24 +6,25 @@ from .models import Event
 
 class PendulumTest(TestCase):
 
+    def setUp(self):
+        self.now = pendulum.now().start_of('day')
+        self.event = Event.objects.create(start=self.now)
+
     def test_datetime_query(self):
-        now = pendulum.now()
 
-        event = Event.objects.create(start=now)
-        self.assertEqual(Event.objects.filter(start=now).count(), 1)
+        self.assertEqual(Event.objects.filter(start=self.now).count(), 1)
 
-        event.refresh_from_db()
-        event.save()
+        self.event.refresh_from_db()
+        self.event.save()
+        # now the text representation in the DB changed to datetime.datetime
 
-        # now its different because the text representation in the DB changed to
-        # the datetime.datetime version
-        self.assertEqual(Event.objects.filter(start=now).count(), 1)
+        # and tho being treated as the same time
+        self.assertEqual(self.event.start, self.now)
+
+        # the ORM cant find my event anymore
+        self.assertEqual(Event.objects.filter(start=self.now).count(), 1)
 
     def test_dates_query(self):
-
-        now = pendulum.now()
-
-        event = Event.objects.create(start=now)
 
         # this is not working since the django function that is filtering out the dates
         # cant handle pendulums datetime strings i guess
@@ -31,8 +32,25 @@ class PendulumTest(TestCase):
         self.assertIn(pendulum.datetime(2018, 1, 1).date(), years)
 
         # but it will work if we do this and test it again
-        event.refresh_from_db()
-        event.save()
+        self.event.refresh_from_db()
+        self.event.save()
 
         years = Event.objects.dates('start', 'year')
         self.assertIn(pendulum.datetime(2018, 1, 1).date(), years)
+
+    def test_weird_time_difference(self):
+
+        self.event.refresh_from_db()
+        self.event.save()
+
+        # now it get even more weird
+        # we have exactly one event in the database
+        self.assertEqual(Event.objects.count(), 1)
+
+        # and the events start time matches, what we used to save it with
+        self.assertEqual(Event.objects.get().start, self.now)
+
+        # so this should not be possible
+        self.assertEqual(Event.objects.filter(start__range=(
+            self.now.subtract(hours=22, minutes=59),
+            self.now.subtract(hours=22))).count(), 0, 'wen shouldnt have an event here')
